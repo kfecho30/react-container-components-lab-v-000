211920838316116d1aae6c76e83b47af684bcc1b
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 02:13:41 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import LatestMovieReviewsContainer from './LatestMovieReviewsContainer.js';
-import SearchableMovieReviewsContainer from './LatestMovieReviewsContainer.js';
 
 export default class MovieReviews extends React.Component{
   render(){
