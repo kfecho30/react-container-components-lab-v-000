8f0a5eba313624b3cb24549a482ecb88facf835e
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 02:07:51 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,6 +7,12 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 export default class LatestMovieReviewsContainer extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      reviews:[],
+    }
+  }
   render(){
     return(
       <div className="latest-movie-reviews">
